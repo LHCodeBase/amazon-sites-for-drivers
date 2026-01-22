28b3e7a5049122fbcb355c9aa266a1dc0a664b73
Kendrick Leiter <lhcodebase@gmail.com>
2026-01-22 03:57:18 -0500

    fix warnings not showing

--- js/load-info.js
+++ js/load-info.js
@@ -4,27 +4,17 @@ const elSite = document.getElementById("site");
 const elWarningsUl = document.getElementById("warnings-ul");
 const elPreferredRoutes = document.getElementById("preferredRoutes");
 
-function createWarningItem(warnings) {
-		if (warnings.length > 0) {
-			for (let i = 0; i < warnings; i++) {
-				elWarningsUl.foreach((e) => {
-					let li = document.createElement("li");
-					li.innerText = warnings[i];
-					elWarningsUl.appendChild(li);
-				})
-			}
-		}
-}
 
 userQuery.addEventListener('change',
 	(e) => {
 		if (siteIds.includes(e.target.value)) {
 			elSite.innerText = e.target.value;
-			// Need to make 'sites' contain dict with key being siteId
-			/* grave notation doesn't work for this
-				* you must use bracket notation */
-			createWarningItem(sites[e.target.value]);
-			// elWarningsUl.innertext = sites[e.target.value].warnings;
+			/* grave notation doesn't work for this you must use bracket notation */
+			sites[e.target.value]["warnings"].forEach((text) => {
+				let li = document.createElement("li");
+				li.innerText = text;
+				elWarningsUl.appendChild(li);
+			})
 		}
 	}
 );
@@ -43,3 +33,4 @@ clearInput.addEventListener('click',
 		userQuery.value = "";
 	}
 );
+
